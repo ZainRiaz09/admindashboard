cd210b76429376558635b3f02771213eb985346b
zain <zainriazali9@gmail.com>
2025-02-11 17:08:39 +0500

    .

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -52,7 +52,7 @@ const SignIn = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/signin', {
+      const response = await fetch('https://admindashboard-xjwk.vercel.app/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
